adb42d4e2567d7ef4d7324cb91ad21e9797174c0
kunalmunur <sinhakunalsinha41@gmail.com>
2025-05-27 12:57:30 +0530

    1st commit

---
+++ src/test/java/m1/A.java
@@ -0,0 +1,12 @@
+package m1;
+
+public class A {
+
+	 
+	public static void main(String[] args) {
+		System.out.println("kunal sinha");
+		System.out.println("i am learning git bash");
+		
+		// modified
+	}
+}
