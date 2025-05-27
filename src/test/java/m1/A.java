e8b502c20acee2b8a3098fc3d5eb069e1cb839fe
kunalmunur <sinhakunalsinha41@gmail.com>
2025-05-27 13:07:30 +0530

    2nd commit

--- src/test/java/m1/A.java
+++ src/test/java/m1/A.java
@@ -6,7 +6,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("kunal sinha");
 		System.out.println("i am learning git bash");
-		
+		System.out.println("i am learning git bash1234");
 		// modified
 		
 		// modified2
