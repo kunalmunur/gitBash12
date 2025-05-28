f1b858df6c5930fcbfa66877553ceb7bf4b133d1
kunalmunur <sinhakunalsinha41@gmail.com>
2025-05-28 15:52:02 +0530

    1

--- src/test/java/m1/A.java
+++ src/test/java/m1/A.java
@@ -6,7 +6,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("kunal sinha");
 		System.out.println("i am learning git bash");
-		System.out.println("i am learning git bash1234");
+		System.out.println("i am learning git bash1234555");
 		// modified
 		
 		// modified2
